ca57c2f8a89977562d2e4ea765193d6fa3cc471e
asifreza-zhcet <asifreza.zhcet@gmail.com>
2024-06-03 10:04:11 +0530

    updated

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import streamlit as st
 import requests
+import streamlit as st
+from gtts import gTTS
 from main import func
 
+
+def text_to_speech(summary):
+    tts = gTTS(summary)
+    tts.save('audio.mp3')
+
 st.title('Text Summary')
-url = st.text_input('enter the url')
-st.write(func(url))
+with st.form(key='my_form'):
+    url = st.text_input("Enter url")
+    frac = st.slider(label='fraction', min_value=0.0, max_value=1.0, step=0.01)
+    submit_button = st.form_submit_button(label='Submit')
+
+speech_button = False
+
+if submit_button:
+    summary = func(url, frac)
+    st.write(summary)
+    speech_button = st.button(label='speech')
+       
+
+if speech_button:
+    text_to_speech(summary=summary)
+    st.audio(data='audio.mp3')
+
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
-def func(url):
+import re
+from string import punctuation
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from heapq import nlargest
+
+stop = stopwords.words('english')
+
+def txt_clean(x):
+    '''this function cleans the text'''
+   
+    x = re.sub('\[.*?\]','', x) #remove [] and anything in between the brackets
+    x = re.sub('\(.*?\)', '',x)
+    x = re.sub('www\S+|https?\S+', '', x) #remove links
+    x = re.sub('\<.*?\>', '', x) #remove html tags
+    x = re.sub(f'[{re.escape(punctuation)}]', '',x) #remove punctuations
+    x = re.sub('\n ','',x) #remove new line
+    x = re.sub('\\xa0', ' ',x)
+    return x
+
+def func(url, n):
     web_page = requests.get(url).text
     soup = BeautifulSoup(web_page, 'html.parser')
 
@@ -9,35 +29,21 @@ def func(url):
     for p_tag in p_tags:
         corpus += p_tag.text + ' '
 
-    import re
-    from string import punctuation
-    def txt_clean(x):
-        x = x.lower() #convert to lower case
-        x = re.sub('\[.*?\]','', x) #remove [] and anything in between the brackets
-        x = re.sub('\(.*?\)', '',x)
-        x = re.sub('www\S+|https?\S+', '', x) #remove links
-        x = re.sub('\<.*?\>', '', x) #remove html tags
-        x = re.sub(f'[{re.escape(punctuation)}]', '',x) #remove punctuations
-        x = re.sub('\n ','',x) #remove new line
-        x = re.sub('\\xa0', ' ',x)
-        return x
-
-    from nltk.tokenize import sent_tokenize, word_tokenize
-    from nltk.corpus import stopwords
-    stop = stopwords.words('english')
+    original = sent_tokenize(corpus)
     clean_sent = [txt_clean(sent) for sent in sent_tokenize(corpus)]
 
 
     word_freq = {}
     for sent in clean_sent:
         for word in word_tokenize(sent):
-            if word in stop:
+            if word.lower() not in stop:
                 try:
-                    word_freq[word] += 1
+                    word_freq[word.lower()] += 1
                 except KeyError:
-                    word_freq[word] = 1
+                    word_freq[word.lower()] = 1
 
     max_freq = max(word_freq.values())
+
     norm_freq = {}
 
     for key, value in word_freq.items():
@@ -49,13 +55,14 @@ def func(url):
     for sent in clean_sent:
         for word in word_tokenize(sent):
             try:
-                score += norm_freq[word]
+                score += norm_freq[word.lower()]
             except KeyError:
                 pass
         sent_score[sent] = score
         score = 0
-    n = 10
+    
+    length = int(len(clean_sent) * n)
 
-    from heapq import nlargest
-    summary = nlargest(n, iterable=sent_score, key=sent_score.get)
+    
+    summary = nlargest(length, iterable=sent_score, key=sent_score.get)
     return '.'.join(summary)
