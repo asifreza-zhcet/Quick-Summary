52a9737268a46da81088f343ab13f3f07e70fa04
asifreza-zhcet <asifreza.zhcet@gmail.com>
2024-06-04 08:18:59 +0530

    new update

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ import requests
 import streamlit as st
 from gtts import gTTS
 from main import func
-
+print('a')
 
 def text_to_speech(summary):
     tts = gTTS(summary)
     tts.save('audio.mp3')
+    st.audio(data='audio.mp3')
+    print('b')
 
 st.title('Text Summary')
 with st.form(key='my_form'):
     url = st.text_input("Enter url")
     frac = st.slider(label='fraction', min_value=0.0, max_value=1.0, step=0.01)
     submit_button = st.form_submit_button(label='Submit')
+    print('c')
 
-speech_button = False
 
 if submit_button:
     summary = func(url, frac)
     st.write(summary)
-    speech_button = st.button(label='speech')
-       
+    speech_button = st.button(label='speech',on_click=text_to_speech,args=(summary,))
+    print('d')   
 
-if speech_button:
-    text_to_speech(summary=summary)
-    st.audio(data='audio.mp3')
+print('*'*50)    
 
